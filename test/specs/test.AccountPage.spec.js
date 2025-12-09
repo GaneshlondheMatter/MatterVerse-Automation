c103532aa5fd9c64590f286dd11ebe559e7543c9
Ganesh Londhe <ganesh.londhe@matter.in>
2025-12-09 20:34:15 +0530

    added wait

--- test/specs/test.AccountPage.spec.js
+++ test/specs/test.AccountPage.spec.js
@@ -1,16 +1,18 @@
 const LoginHelper = require('../../Helper/login.helper');
 const matterHomePage = require('../pageobjects/matterHome.page');
-const assertionHelper = require('../../Helper/Assert.helper');  
+const assertionHelper = require('../../Helper/Assert.helper');
 
 describe('Login Flow Automation', () => {
 
     it('Should login successfully using valid phone number and OTP', async () => {
 
         await LoginHelper.login("8780981556", "8319");
+        await browser.pause(6000);
         await matterHomePage.clickMatterHomeLogo();
+        await browser.pause(6000);
         await matterHomePage.clickMyMatterTab();
+        await browser.pause(6000);
         await matterHomePage.clickAccountTab();
-
     });
 
 });
--- test/specs/test.uploadDrivingLicense.spec.js
+++ test/specs/test.uploadDrivingLicense.spec.js
@@ -9,15 +9,19 @@ describe("Document Upload Driving License", () => {
     it("Should upload front & back side of Driving License successfully", async () => {
 
         await LoginHelper.login("8780981556", "8319");
+         await browser.pause(6000);
         await matterHomePage.clickMatterHomeLogo();
+         await browser.pause(6000);
         await matterHomePage.clickMyMatterTab();
+         await browser.pause(6000);
         await matterHomePage.clickAccountTab();
-        await browser.pause(5000);
-
+         await browser.pause(6000);
         // Step 1: Navigate to Vehicle Settings → Documents
         await VehicleSettingsPage.clickVehicleSettingsLink();
-        await VehicleSettingsPage.clickDocumentsLink();
+        await browser.pause(5000);
 
+        await VehicleSettingsPage.clickDocumentsLink();
+        await browser.pause(5000);
         // Step 2: Open Driving License
         await DrivingLicensePage.openDrivingLicenseSection();
         // If already uploaded, delete both sides first
@@ -25,9 +29,13 @@ describe("Document Upload Driving License", () => {
             await DrivingLicensePage.deleteFrontSide();
             await DrivingLicensePage.deleteBackSide();
         }
+        await browser.pause(5000);
+
         // Step 3: Upload Front and Back Side
         await DrivingLicensePage.uploadFrontSide(false);
         await DrivingLicensePage.uploadBackSide(false);
+        await browser.pause(5000);
+
         // Step 4: modify both sides
         if (await DrivingLicensePage.DrivingFrontSideUploaded.isDisplayed() && await DrivingLicensePage.DrivingBackSideUploaded.isDisplayed()) {
             await DrivingLicensePage.FrontPageModifyBtn();
